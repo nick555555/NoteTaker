c97e8b464aea5df8e2c056555641902538a10b37
Nick <57273851+nick555555@users.noreply.github.com>
2020-02-25 22:36:10 -0800

    html routes working

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require("express");
-const app = express();
-// const fs = require("fs");
+const fs = require("fs");
 const path = require("path");
+
+//Sets up the Express App
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+//html routes
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+//Starts the server to begin listening 
+app.listen(PORT, function () {
+    console.log("App listening on PORT" + PORT);
+});
