47f7bc45de8659673444839c14c1aa04b79ddf7d
Nick <57273851+nick555555@users.noreply.github.com>
2020-02-25 23:35:29 -0800

    added api routes

--- server.js
+++ server.js
@@ -1,25 +1,55 @@
-const express = require("express");
+var express = require("express");
+var path = require("path");
 const fs = require("fs");
-const path = require("path");
 
-//Sets up the Express App
-const app = express();
-const PORT = process.env.PORT || 3000;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-//Sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
 
 //html routes
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-//Starts the server to begin listening 
+//api routes
+db = require("./db/db.json");
+app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+app.post("/api/notes", function (req, res) {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        let newData = JSON.parse(data);
+        newData.push(req.body)
+        fs.writeFile("./db/db.json", JSON.stringify(newData), (err) => {
+            if (err) throw err;
+            res.send('Posted')
+        })
+    });
+});
+app.delete("/api/notes/:id", function (req, res) {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        let newData = JSON.parse(data);
+        newData.splice(req.params.id, 1)
+        fs.writeFile("./db/db.json", JSON.stringify(newData), (err) => {
+            if (err) throw err;
+            res.send('Deleted')
+        })
+    })
+});
+
+//server listening 
 app.listen(PORT, function () {
     console.log("App listening on PORT" + PORT);
 });
